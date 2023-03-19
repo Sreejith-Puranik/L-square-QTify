07f8acd616ce3d06c214be86b3865cf5a856b29f
sreejith <sree.apk@gmail.com>
2023-03-19 23:05:32 +0530

    updated

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -22,6 +22,7 @@ export default function Section(props) {
         setFilters([...filters, ...data]);
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const filteredcards = cards.filter((card) =>
